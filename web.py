56f74f695e6842ed89af26a9957f728d0410ae99
avost <noasueur88@gmail.com>
2026-04-21 14:46:58 +0200

    Update web.py

--- web.py
+++ web.py
@@ -61,4 +61,6 @@ def index():
 
     return render_template("index.html", roles=roles)
 
-app.run(port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
